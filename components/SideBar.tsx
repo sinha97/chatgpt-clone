4eb4c4542812d7129f6c0e2cc7f5f63ba8932859
sinha97 <vk9633698@gmail.com>
2023-02-10 11:51:29 +0530

    add signout functionality

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,15 +1,27 @@
-import NewChat from "./NewChat"
+"use client";
+
+import NewChat from "./NewChat";
+import { useSession, signOut } from "next-auth/react";
 
 const SideBar = () => {
+  const { data: session } = useSession();
   return (
-      <div className="p-2 flex flex-col h-screen">
-          <div className="flex-1">
-              <div>
-                  <NewChat />
-              </div>
-          </div>
+    <div className="p-2 flex flex-col h-screen">
+      <div className="flex-1">
+        <div>
+          <NewChat />
+        </div>
       </div>
-  )
-}
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt="Profile pic"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+        />
+      )}
+    </div>
+  );
+};
 
-export default SideBar
+export default SideBar;
